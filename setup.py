0b138260de42294756af9b1f63496f73aa1edc21
loind <loind@vccloud.vn>
2021-06-22 14:20:40 +0700

    update requirement

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bizfly_kafka_notification",
-    version="0.0.0.1",
     author="BizFly Cloud",
     author_email="loinguyenduc@vccorp.vn",
+    name="bizfly_kafka_notification",
+    version="0.0.0.1",
     description="BizFly Cloud DevTeam Client in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/ducloi3105/kafka_notify.git",
     packages=setuptools.find_packages(),
     install_requires=[
         'kafka-python==2.0.2',
